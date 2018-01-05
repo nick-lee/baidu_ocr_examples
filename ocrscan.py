1207668b47fab4b0f5c66fa57aafbf376428196a
nick lee <2074999@qq.com>
2018-01-05 05:04:26 +0000

    beta version for table scan can work correctly

--- ocrscan.py
+++ ocrscan.py
@@ -1,18 +1,17 @@
 #coding = utf-8
 from aip import AipOcr
 import sys
+import time
 import os
 import click
 import io,json
 import datetime
+import wget
 from pprint import pprint
 
 # fixme : replace it with user's key
-APP_ID = 'USER APP ID'
-API_KEY = 'USER API KEY'
-SECRET_KEY = 'USER SECRET KEY'
 
-aipOcr = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+#aipOcr = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
 options = {
   'detect_direction': 'true',
@@ -30,83 +29,106 @@ class LogData(object):
         self.url = 'NOT DEFINE'
         self.result = 'NOT DEFINE'
 
+class AipConf(object):
+    def __init__(self):
+        self.app_id = None
+        self.api_key = None
+        self.secret_key = None
+
 def load_img_file(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
-def load_conf():
+# support input parameter is base on seconds
+def sleep_with_dots(s):
+    sleeping_count = s * 2
+    sys.stdout.write('  ')
+    for count in range(0,sleeping_count):
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.5)
+    print(" ")
+
+def get_file_by_url(url,outpath):
+    sys.stdout.write('  ')
+    return wget.download(url,outpath)
+
+def load_conf(aip_conf):
     try:
         data = json.load(open('./aip.conf'))
-        pprint(data)
-        APP_ID = data["aip"]["app_id"]
-        API_KEY = data["aip"]["api_key"]
-        SECRET_KEY = data["aip"]["secret_key"]
-        print("Connect Server with APP_ID[%s] API_KEY[%s] SECRET_KEY[%s] " % (APP_ID,API_KEY ,SECRET_KEY) )
+        #pprint(data)
+        aip_conf.app_id = data["aip"]["app_id"]
+        aip_conf.api_key = data["aip"]["api_key"]
+        aip_conf.secret_key = data["aip"]["secret_key"]
     except Exception as err:
         raise ConfNotExistErr('Configuration not be handled well!')
 
 
-def handle_table_ocr(f):
+def handle_table_ocr(client,f):
     # 1. make a request ; 2. get the result
     # image size < 4M and width|height > 15px , max widht/height < 4096px
  
     msg_back = {}
+    request = None
     try:
-        client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
         logdata = LogData(f,datetime.datetime.now())
+        print("  load content from %s and send request to server" % (os.path.basename(f)))
         content = load_img_file(f)
-        msb_back = client.tableRecognitionAsync(content)
+        msg_back = client.tableRecognitionAsync(content)
     except Exception as err:
+        print "error!"
         print err
+
     try:
-        print msg_back["result"]["result_id"] 
-        print msg_back["log_id"]
+        #print(msg_back)
+        print('  server processing , request_id = %s ' % (msg_back['result'][0]['request_id']))
     except Exception as err:
         print err
-        print("AIP return error , LOG_ID[%s] ERROR_MSG[%s]" % (msg_back["log_id"], msg_back["error_msg"]))
-        logdata.result='Request Error'
+        print(msg_back)
         return 
-     
+
+    # timeout value request because if ask result so quickly, the server may not handle it in time!
+    print("  wating server to process")
+    sleep_with_dots(10)
     try:
+        print("  get result request_id = %s " % (msg_back['result'][0]['request_id']))
         options = {}
         ocr_back = {}
         # type could be json | excel
         options["result_type"] = "excel"
-        ocr_back = client.getTableRecognitionResult(msg_back['result']['result_id'])
-	logdata.result = ocr_back["ret_msg"]
+        ocr_back = client.getTableRecognitionResult(msg_back['result'][0]['request_id'],{'result_type':'excel'})
+        #print(ocr_back)
+        print("  server return status : %s " % ocr_back['result']['ret_msg'])
+        #print("url : %s " % ocr_back['result']['result_data'])
+        filename = get_file_by_url(ocr_back['result']['result_data'], './output/table')
+        print('\n  saving %s to output/table' % filename)
+	#logdata.url = ocr_back['result']["result_data"]
         if (options["result_type"] == "excel"):
-            logdata.url = ocr_back['result']['result_data']['file_rul']
+            logdata.url = ocr_back['result']['result_data']
         else:
             logdata.url = 'JSON NOT DEFINE'
         logdata.result='Success'
-        pprint(ocr_back)
+        #pprint(ocr_back)
     except Exception as err:
         print err
-        print("AIP return error , LOG_ID[%s] ERROR_MSG[%s]" % (msb_back['log_id'],msg_back['error_msg']) )
-        logdata.result='Result Error'
+        print("AIP return error , REQUEST_ID[%s] ERROR_MSG[%s]" % (msb_back['result'][0]['request_id'],ocr_back['result']['error_msg']) )
+        logdata.result='get the %s result error'
         return 
 
     
 
-def handle_word_ocr(f):
+def handle_word_ocr(client,f):
     pass
 
-def handle_ocr(c,path):
-    try:
-        load_conf()
-    except ConfNotExistErr:
-        print "Configuration file not correct!"
-        return
-
+def handle_ocr(client,c,path):
     if c in ['table']:
-    	print("=>scan as table")
-        handle_table_ocr(path)
+    	print("=>scan %s as table" % os.path.basename(path) )
+        handle_table_ocr(client,path)
     else:
     	print("=>scan as word")
-        handle_word_ocr(path)  
-    print "=>processing %s" % (path)    
+        handle_word_ocr(client,path)  
+    print "=>processing %s end." % (os.path.basename(path))    
 
-    print("=>end.")
 
 
 @click.command()
@@ -124,26 +146,34 @@ def main(c,f,o,d):
         print "Please choose the file or directory, use --help for detail" 
         return
 
+
+    aipconf = AipConf()
+    try:
+        load_conf(aipconf)
+    except ConfNotExistErr:
+        print "Configuration file not correct!"
+        return
+
+    client = AipOcr(aipconf.app_id, aipconf.api_key,aipconf.secret_key)
+    pprint(client)
+    print("Connect Server with APP_ID[%s] API_KEY[%s] SECRET_KEY[%s] " % (aipconf.app_id,aipconf.api_key ,aipconf.secret_key) )
     if (d):
         file_count = 0
         for filename in os.listdir(d):
-    	    if filename.endswith(".png") or filename.endswith(".jpg"): 
-    	        file_count=file_count+1
+            if filename.endswith(".png") or filename.endswith(".jpg"): 
+                file_count=file_count+1
                 print(os.path.join(d, filename))
                 filepath = os.path.join(d, filename)
-                handle_ocr(c,filepath)
+                handle_ocr(client,c,filepath)
     	    else:
-        	    continue
+                continue
         if file_count == 0:
             print "Doesn't find any valid files under %s " % d
         else:
             print "%d files are processed!" % file_count
     else:
-        handle_ocr(c,f)
+        handle_ocr(client,c,f)
 
- 
-
-    
 if __name__ == '__main__':
     main()
 
